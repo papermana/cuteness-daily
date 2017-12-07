ff0cd853e15f02c95563da311f821c325ecc99ad
Dominik Rowicki <dominik.rowicki@netguru.co>
2017-12-07 20:05:44 +0100

    Add support for events and respond to sad messages

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ const schedule = require('node-schedule');
 const routes = {
   root: '/',
   oAuth: '/oauth',
+  events: '/events',
 };
 
 const collections = {
   integrations: 'integrations',
+  config: 'config',
 };
 
 
@@ -52,6 +54,11 @@ const initializeApp = () => {
 const db = initializeDb();
 const app = initializeApp();
 
+const messageQueue = {
+  queue: [],
+  processing: false,
+};
+
 
 /*  UTILS  */
 
@@ -67,13 +74,20 @@ const makeUrl = (url, params) => {
   return `${url}?${stringifiedParams}`;
 };
 
+const isSadMessage = message => {
+  return (
+    message.includes(':(')
+      || message.includes('sad')
+  );
+}
+
 
 /*  API  */
 
 const authorize = () => {
   const params = {
     client_id: process.env.SLACK_CLIENT_ID,
-    scope: 'chat:write:bot',
+    scope: 'chat:write:bot,channels:history',
     redirect_uri: getRouteUrl('oAuth'),
   };
 
@@ -130,6 +144,25 @@ const postMessage = (url, { url: picUrl, htmlUrl, author }) => {
   });
 }
 
+const postResponseToSadMessage = (url, userId) => {
+  return findAnimalPic()
+    .then(({ url: picUrl, htmlUrl, author }) => {
+      return fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          attachments: [{
+            pretext: `I'm really sorry to see you sad, <@${userId}>. Here, have this pic of a cute animal`,
+            image_url: picUrl,
+            footer: `By <${htmlUrl}|${author}> from <unsplash.com|Unsplash>`,
+          }],
+        }),
+      });
+    });
+};
+
 const sendAnimalPics = () => {
   const getPic = findAnimalPic();
   const getCollections = db
@@ -144,6 +177,42 @@ const sendAnimalPics = () => {
       });
     });
 }
+
+
+/*  MESSAGE QUEUE HANDLING  */
+
+const processMessageQueue = () => {
+  const data = messageQueue.queue.shift();
+  const {
+    team_id: teamId,
+    event: { text, user },
+  } = data;
+
+  if (isSadMessage(text)) {
+    db
+      .collection(collections.integrations)
+      .doc(teamId)
+      .get()
+      .then(coll => {
+        postResponseToSadMessage(coll.get('incoming_webhook').url, user);
+      });
+  }
+
+  if (messageQueue.queue.length) {
+    processMessageQueue();
+  } else {
+    messageQueue.processing = false;
+  }
+}
+
+const addToMessageQueue = data => {
+  messageQueue.queue.push(data);
+
+  if (!messageQueue.processing) {
+    messageQueue.processing = true;
+    processMessageQueue();
+  }
+};
 
 
 /*  ROUTES  */
@@ -178,7 +247,8 @@ app.get(routes.oAuth, (req, res) => {
     .then((data) => {
       db
         .collection(collections.integrations)
-        .add(data);
+        .doc(data.team_id)
+        .set(data);
 
       return data.incoming_webhook.url;
     })
@@ -190,6 +260,56 @@ app.get(routes.oAuth, (req, res) => {
           postMessage(webhookUrl, picData);
         });
     });
+});
+
+app.post(routes.events, (req, res) => {
+  if (req.body.type === 'url_verification') {
+    db
+      .collection(collections.config)
+      .doc('eventsToken')
+      .set({ token: req.body.token });
+    res.status(200);
+    res.send(req.body.challenge);
+  }
+
+  const getToken = db
+        .collection(collections.config)
+        .doc('eventsToken')
+        .get();
+
+  getToken.then(data => {
+    const token = data.get('token');
+
+    if (token !== req.body.token) {
+      return;
+    }
+
+    if (req.body.type !== 'event_callback') {
+      res.status(200);
+      res.send();
+      return;
+    }
+
+    if (
+      req.body.event.type === 'message'
+        && req.body.event.subtype !== 'bot_message'
+    ) {
+      addToMessageQueue(req.body);
+
+      res.status(200);
+      res.send();
+    }
+
+    if (req.body.event.type === 'app_uninstalled') {
+      db
+        .collection(collections.integrations)
+        .doc(req.body.team_id)
+        .delete();
+
+      res.status(200);
+      res.send();
+    }
+  });
 });
 
 
